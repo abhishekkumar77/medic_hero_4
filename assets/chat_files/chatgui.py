6868e4894a89fee4f221f50535c0bf68c8f9de2d
sunny <ghtdhtrdjr@tf.hv>
2020-04-21 18:16:02 +0530

    chat engine update

--- assets/chat_files/chatgui.py
+++ assets/chat_files/chatgui.py
@@ -1,13 +1,14 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
-from keras.models import load_model
-model = load_model('chatbot_model.h5')
 import json
 import random
+from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+
+model = load_model('chatbot_model.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
@@ -17,8 +18,9 @@ intents2 = json.loads(open('symp.json').read())
 words2 = pickle.load(open('words2.pkl','rb'))
 classes2 = pickle.load(open('classes2.pkl','rb'))
 
-flag=0
-
+context=1
+dis=""
+user=[]
 
 def clean_up_sentence(sentence):
     # tokenize the pattern - split words into array
@@ -82,87 +84,103 @@ def predict_class2(sentence, model):
     for r in results:
         return_list.append({"intent": classes2[r[0]], "probability": str(r[1])})
     return return_list
+
+def get_med_Response_disease(ints,intents_json, symp, msg):
+    global dis
+    global context
+    #tag = ints[0]['intent']
+    tag=dis
+    list_of_intents = symp['intents']
+    context=1
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
     
-def get_med_Response(ints, intents_json,symp, msg):
+def get_med_Response_temp(msg):    
+    global dis
+    global context
+    context=4
+    user.append(msg)
+    fever=0
+    for mesg in user:
+        for wrd in mesg.split():
+            if(wrd.isdigit()):
+                fever=int(wrd)
+            
+    if(fever!=0):
+        reply="you got "+str(fever)+" fever ?"
+    else:
+        reply = "Is your body temperature above 98 ?"
+    return reply
+    
+   
+    
+def get_med_Response(ints,intents_json, symp, msg):
+    global context
+    global dis
     tag = ints[0]['intent']
     list_of_intents = symp['intents']
     for i in list_of_intents:
-        if(i['tag']== tag):            
+        if(i['tag']== tag):
+            context=3
+            dis=tag                
             result = random.choice(i['responses'])
             break
     return result
 
 def getResponse(ints, intents_json,symp, msg):
+    global context
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag']== tag):
             if(tag=="diagonised"):
-                flag=1
+                context=2
                 ints2 = predict_class2(msg, model2)
-                result = get_med_Response(ints2, intents_json,symp, msg)
+                result = get_med_Response(ints2, intents_json,symp, msg)     
+                result = "any more known symptomes?"
                 break
             result = random.choice(i['responses'])
             break
     return result
 
-def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents,intents2,msg)
+def chatbot_response(ctx,msg):
+    global user
+    global context
+    global dis
+    context = ctx
+    user.append(msg)
+
+    
+    if(ctx==1):
+        ints = predict_class(msg, model)
+        res = getResponse(ints, intents,intents2,msg)
+    elif (ctx==2):
+        ints = predict_class2(msg,model2)
+        res = get_med_Response(ints,intents,intents2, msg) 
+    elif(ctx==3):
+        #ints = predict_class2(msg,model2)
+        #res = get_med_Response_temp(ints,intents,intents2, msg)
+        res = get_med_Response_temp(msg)
+    elif(ctx==4):
+        ints = predict_class2(msg,model2)
+        res = get_med_Response_disease(ints,intents,intents2, msg)
+        
     return res
 
 
-#Creating GUI with tkinter
-import tkinter
-from tkinter import *
-
-yes_list=["yes"]
-no_list=["no"]
-
-def send():
-    msg = EntryBox.get("1.0",'end-1c').strip()
-    EntryBox.delete("0.0",END)
-
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
-    
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
-            
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
- 
-
-base = Tk()
-base.title("Medic Hero 4 (testing)")
-base.geometry("400x500")
-base.resizable(width=FALSE, height=FALSE)
-
-#Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
-
-ChatLog.config(state=DISABLED)
-
-#Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
-
-#Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                    command= send )
-
-#Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
-scrollbar.place(x=376,y=6, height=386)
-ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+while(1):
+    con = int(input("Context:"))
+    msg = str(input("Msg:"))
+    response = chatbot_response(con,msg)
+    c = str(context)
+    print("Context:"+c+"  Bot:"+response)
 
-base.mainloop()
+
+
+#---]medichero[---#
+
--- home/views.py
+++ home/views.py
@@ -4,20 +4,16 @@ from django.views.decorators.csrf import csrf_exempt
 import os
 from medic_hero4.settings import BASE_DIR
 from django.http import JsonResponse
-
-
-
 import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
 from keras.models import load_model
-#path = "C:\Users\abhishek\MH4\medic_hero4\assets\chat_files\chatbot_model.h5"
-model = load_model("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\chatbot_model.h5")
 import json
 import random
+
+model = load_model("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\chatbot_model.h5")
 intents = json.loads(open("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\intents.json").read())
 words = pickle.load(open("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\words.pkl",'rb'))
 classes = pickle.load(open("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\classes.pkl",'rb'))
@@ -27,8 +23,9 @@ intents2 = json.loads(open("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_
 words2 = pickle.load(open("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\words2.pkl",'rb'))
 classes2 = pickle.load(open("C:\\Users\\abhishek\\MH4\\medic_hero4\\assets\\chat_files\\classes2.pkl",'rb'))
 
-flag=0
-
+context=1
+dis=""
+user=[]
 
 def clean_up_sentence(sentence):
     # tokenize the pattern - split words into array
@@ -93,33 +90,83 @@ def predict_class2(sentence, model):
         return_list.append({"intent": classes2[r[0]], "probability": str(r[1])})
     return return_list
     
-def get_med_Response(ints, intents_json,symp, msg):
+def get_med_Response_disease(ints,intents_json, symp, msg):
+    global dis
+    global context
+    #tag = ints[0]['intent']
+    tag=dis
+    list_of_intents = symp['intents']
+    context=1
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+    
+def get_med_Response_temp(msg):    
+    global dis
+    global context
+    context=4
+    user.append(msg)
+    fever=0
+    for mesg in user:
+        for wrd in mesg.split():
+            if(wrd.isdigit()):
+                fever=int(wrd)            
+    if(fever!=0):
+        reply="you got "+str(fever)+" fever ?"
+    else:
+        reply = "Is your body temperature above 98 ?"
+    return reply  
+    
+def get_med_Response(ints,intents_json, symp, msg):
+    global context
+    global dis
     tag = ints[0]['intent']
     list_of_intents = symp['intents']
     for i in list_of_intents:
-        if(i['tag']== tag):            
+        if(i['tag']== tag):
+            context=3
+            dis=tag                
             result = random.choice(i['responses'])
             break
     return result
 
 def getResponse(ints, intents_json,symp, msg):
-    global flag
+    global context
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag']== tag):
             if(tag=="diagonised"):
-                flag=1
+                context=2
                 ints2 = predict_class2(msg, model2)
-                result = get_med_Response(ints2, intents_json,symp, msg)
+                result = get_med_Response(ints2, intents_json,symp, msg)     
+                result = "any more known symptomes?"
                 break
             result = random.choice(i['responses'])
             break
     return result
 
-def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents,intents2,msg)
+def chatbot_response(ctx,msg):
+    global user
+    global context
+    global dis
+    context = ctx
+    user.append(msg)    
+    if(ctx==1):
+        ints = predict_class(msg, model)
+        res = getResponse(ints, intents,intents2,msg)
+    elif (ctx==2):
+        ints = predict_class2(msg,model2)
+        res = get_med_Response(ints,intents,intents2, msg) 
+    elif(ctx==3):
+        #ints = predict_class2(msg,model2)
+        #res = get_med_Response_temp(ints,intents,intents2, msg)
+        res = get_med_Response_temp(msg)
+    elif(ctx==4):
+        ints = predict_class2(msg,model2)
+        res = get_med_Response_disease(ints,intents,intents2, msg)        
     return res
 
 
@@ -127,11 +174,15 @@ def chatbot_response(msg):
 # Create your views here.
 
 @csrf_exempt
-def reply(request):    
+def reply(request):
+    global context    
     x = request.POST["msg"]
-    resp = chatbot_response(x)          
+    ctx = request.POST["ctx"]
+    ctx = int(ctx)
+    resp = chatbot_response(ctx,x)          
     print(resp)
-    return HttpResponse(resp)
+    print(ctx)
+    return HttpResponse(json.dumps({"m":resp,"c":context}),content_type='application/json')
 
 
 def home(request):
